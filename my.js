68d44d4a189e16572c2a71d29e683ec341e26a8e
david <davmwa90@gmail.com>
2022-12-07 17:24:43 +0300

    Completed a functions assignment

---
+++ my.js
@@ -0,0 +1,23 @@
+
+// fetching data from endpoint and displaying on window using DOM manipulation 
+// url = [baseurl + endpoint]  
+function createElement() {
+    let baseURL = "https://jsonplaceholder.typicode.com";
+    let postsEndpoint = "/posts";
+    
+
+    let url = `${baseURL}${postsEndpoint}`;
+    console.log(url);
+    
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>  {
+      data.forEach(post => {
+        const markup = `<li>${post.title}</li>`; 
+        document.querySelector("ul").insertAdjacentHTML('beforeend', markup)
+      })
+      console.log(data)
+    })
+  }
+  createElement(); 
+    
